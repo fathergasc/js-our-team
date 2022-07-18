4c02ad51bf161623805be6388583d766fa76fc21
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-18 15:51:05 +0200

    printed array ourTeam in DOM

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
+containerDom = document.getElementById('container');
+
 const ourTeam = [
   {
-    name: "Wayne Barrett",
-    position: "Founder & CEO",
-    image: "wayne-barnett-founder-ceo.jpg",
+    name: 'Wayne Barnett',
+    position: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
   },
 
   {
-    name: "Angela Caroll",
-    position: "Chief Editor",
-    image: "angela-caroll-founder-ceo.jpg",
+    name: 'Angela Caroll',
+    position: 'Chief Editor',
+    image: 'angela-caroll-founder-ceo.jpg',
   },
 
   {
-    name: "Walter Gordon",
-    position: "Office Manager",
-    image: "walter-gordon-office-manager.jpg",
+    name: 'Walter Gordon',
+    position: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
   },
 
   {
-    name: "Angela Lopez",
-    position: "Social Media Manager",
-    image: "angela-lopez-social-media-manager.jpg",
+    name: 'Angela Lopez',
+    position: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
   },
 
   {
-    name: "Scott Estrada",
-    position: "Developer",
-    image: "scott-estrada-developer.jpg",
+    name: 'Scott Estrada',
+    position: 'Developer',
+    image: 'scott-estrada-developer.jpg',
   },
 
   {
-    name: "Barbara Ramos",
-    position: "Graphic Designer",
-    image: "barbara-ramos-graphic-designer.jpg",
+    name: 'Barbara Ramos',
+    position: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+for (let key in ourTeam) {
+    console.log(key);
+    console.log(ourTeam[key]);
+}
+
+
+for (let i = 0; i < ourTeam.length; i++) {
+    const teamMember = ourTeam[i];
+    console.log('teamMember: ', teamMember);
+    containerDom.innerHTML += `Nome: ${teamMember.name}. Posizione: ${teamMember.position}. Foto: ${teamMember.image}. `;
+}
+
