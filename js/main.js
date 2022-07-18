ee4894dc148ae68188401effc3ddd547fbff42f8
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-18 14:53:20 +0200

    added array ourTeam with all info

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const ourTeam = [
+  {
+    name: "Wayne Barrett",
+    position: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  },
+
+  {
+    name: "Angela Caroll",
+    position: "Chief Editor",
+    image: "angela-caroll-founder-ceo.jpg",
+  },
+
+  {
+    name: "Walter Gordon",
+    position: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+
+  {
+    name: "Angela Lopez",
+    position: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg",
+  },
+
+  {
+    name: "Scott Estrada",
+    position: "Developer",
+    image: "scott-estrada-developer.jpg",
+  },
+
+  {
+    name: "Barbara Ramos",
+    position: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+  },
+];
